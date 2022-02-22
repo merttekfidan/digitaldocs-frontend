3ba5432d07e4865015e38baf3826b50b11436ee5
Merdjan Tekfidan <merttekfidan@gmail.com>
2022-02-22 22:27:31 +0100

    Init api

--- src/api.js
+++ src/api.js
@@ -2,10 +2,32 @@ import axios from 'axios'
 export default class Api {
     constructor(){
         this.api_url = process.env.REACT_APP_ENDPOINT;
+        this.client = null;
+    }
+    init = () => {
+        let headers = {
+            Accept: "application/json",
+        };
+        this.client = axios.create({
+            baseURL: this.api_url,
+            timeout: 31000,
+            headers: headers,
+        });
+        return this.client;
     }
 
-    getAllDocs = async () =>{
-        return await axios.get(this.api_url);
+    getAllDocs = () =>{
+        let route = ''
+        return this.init().get('/documents');
+    }
+
+    getDoc = (param) => {
+        return this.init().get(`/documents/${param}`)
+    }
+
+    createDoc = () => {
+        let route = ''
+        return ''
     }
 
 }
